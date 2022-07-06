d2299ab08883a05c223db0413cc2c1a9b15f6cd2
littlelee2022 <eun0lee@gmail.com>
2022-07-06 11:52:56 +0900

    commit

--- try1.py
+++ try1.py
@@ -1,13 +1,21 @@
 # try1.py
 
 # 함수 정의 
-def devide(a,b):
+def divide(a,b):
     return a/b
 
-#함수 호출
-result = divide(5,0)
+try:
+    #함수 호출
+    result = divide(5,"ㅂㅂㅂ")
+except ZeroDivisionError:
+    print("0으로 나누면 안됩니다")
+except TypeError:
+    print("숫자이여야 합니다")
+else: 
+    print("결과 : {0}".format(result))
 
-print("결과 : {0}".format(result))
+finally:
+    print("무조건 한번 더 실행")
 
 
 print("전체 코드 실행 종료")
