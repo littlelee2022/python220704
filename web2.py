2a3d5a4704b10ab87493f0c1aebcd707e4b72ba0
littlelee2022 <eun0lee@gmail.com>
2022-07-07 12:21:27 +0900

    웹크롤링

--- web2.py
+++ web2.py
@@ -13,3 +13,14 @@ soup = BeautifulSoup(data,"html.parser")
 # <td class="title">
 # <a href="/webtoon/detail?titleId=20853&no=50&weekday=fri" onclick="nclk_v2(event,'lst.title','20853','50')">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
 # </td> 
+
+cartoons = soup.find_all("td",class_="title")
+print("갯수: {0}".format(len(cartoons)))
+title = cartoons[0].find("a").text
+link = cartoons[0].find("a")["href"]
+print(title)
+print(link)
+
+for item in cartoons:
+    title = item.find("a").text
+    print(title.strip())
